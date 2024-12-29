8b8d290d98a3dafa4aad2242b00d2c5512db7c5f
Kirill Ivanchin <chvik1@yandex.ru>
2024-12-29 13:41:12 +0500

    service level and creating jwt

---
+++ internal/lib/jwt/jwt.go
@@ -0,0 +1,13 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"sso/internal/domain/models"
+	"time"
+)
+
+func NewToken(user models.User, app models.App, duration time.Duration) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+}
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -3,8 +3,12 @@ package auth
 import (
 	"context"
 	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
+	"sso/internal/lib/logger/sl"
+	"sso/internal/storage"
 	"time"
 )
 
@@ -21,7 +25,7 @@ var (
 )
 
 type UserSaver interface {
-	SaveUser(ctx context.Context, email string, passHash []byte) (int64, error)
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
 }
 
 type UserProvider interface {
@@ -44,11 +48,61 @@ func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appPr
 }
 
 func (auth *Auth) Login(ctx context.Context, email, password string, appId int32) (*models.User, error) {
-	panic("implement me")
+	const op = "auth.Login"
+
+	log := auth.log.With(slog.String("op", op), slog.String("username", email))
+
+	log.Info("logging user")
+
+	user, err := auth.usrProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			auth.log.Warn("user not found")
+
+			return nil, fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+
+		auth.log.Warn("failed to get user", sl.Err(err))
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err = bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		auth.log.Info("invalid credentials", sl.Err(err))
+
+		return nil, fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := auth.appProvider.App(ctx, appId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	auth.log.Info("user logged in successfully")
 }
 
-func (auth *Auth) RegisterNewUser(ctx context.Context, email, password string) (*models.User, error) {
-	panic("implement me")
+func (auth *Auth) RegisterNewUser(ctx context.Context, email, password string) (int64, error) {
+	const op = "auth.RegisterNewUser"
+
+	log := auth.log.With(slog.String("op", op), slog.String("email", email))
+
+	log.Info("registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash", sl.Err(err))
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := auth.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save new user", sl.Err(err))
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
 }
 
 func (auth *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
